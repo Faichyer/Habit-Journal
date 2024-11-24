249bd2404cc13da94c64a9892cc73172f91d0a04
Miki Namsrai <miki.namsrai@gmail.com>
2024-11-24 19:51:19 +0100

    Fix: not founds

--- app/(dashboard)/habits/not-found.tsx
+++ app/(dashboard)/habits/not-found.tsx
@@ -7,8 +7,8 @@ export default function NotFound({}: Props) {
     return (
         <main className='text-center'>
             <h2 className="text-3xl">We hit a break wall</h2>
-            <p>We couldn`&apos`t find ur page</p>
-            <p>Go back to the <Link href='/habits'>Habits</Link> </p>
+            <p>We couldn't find ur habit</p>
+            <p>Go back to the <Link className={'text-primary'} href='/habits'>Habits</Link> </p>
 
         </main>
     )
--- app/(dashboard)/not-found.tsx
+++ app/(dashboard)/not-found.tsx
@@ -7,7 +7,7 @@ function NotFound({}: Props) {
     return (
         <main className='text-center'>
             <h2 className="text-3xl">There was a problem.</h2>
-            <p>We couldn`&apos`t find ur page</p>
+            <p>We couldn't find ur page</p>
             <p>Go back to the <Link href='/'>Dashboard</Link> </p>
 
         </main>
