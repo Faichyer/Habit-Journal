d95739135e13cba8ddfbb36b0ed379309d06047d
Miki Namsrai <miki.namsrai@gmail.com>
2024-11-24 19:56:30 +0100

    Fix: title not shown on mobile

--- components/typography/page-title.tsx
+++ components/typography/page-title.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 type TitleProps = {
-	title: string;
+    title: string;
 };
 
-const PageTitle = ({ title }: TitleProps) => {
-	return (
-		<h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] hidden md:block">
-			{title}
-		</h1>
-	);
+const PageTitle = ({title}: TitleProps) => {
+    return (
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] md:block">
+            {title}
+        </h1>
+    );
 };
 
 export default PageTitle;
